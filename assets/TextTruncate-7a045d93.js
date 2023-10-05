9c27c77ad0c4162a836ffa7c51b28f0c618b0c69
maysolanki26 <mayur@sarvadhi.com>
2023-10-05 18:42:32 +0530

    new build

---
+++ assets/TextTruncate-7a045d93.js
@@ -0,0 +1,1 @@
+import{j as c}from"./index-3b75abce.js";const a=({text:r,maxLength:s})=>{const n=(r==null?void 0:r.length)>s?r.slice(0,s)+"...":r;return c.jsx("span",{children:n})};export{a as T};
---
+++ assets/useUncertainRef-a63c0b8a.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-3b75abce.js";function u(e){const r=n.useRef(null);return e||r}export{u};
