cd5a90c6f7d185205337b74fbed8a089499c4616
maysolanki26 <mayur@sarvadhi.com>
2023-10-12 13:41:38 +0530

    new build

---
+++ assets/TextTruncate-f64f0cbd.js
@@ -0,0 +1,1 @@
+import{j as c}from"./index-59d5b448.js";const a=({text:r,maxLength:s})=>{const n=(r==null?void 0:r.length)>s?r.slice(0,s)+"...":r;return c.jsx("span",{children:n})};export{a as T};
---
+++ assets/useThemeClass-95bae207.js
@@ -0,0 +1,1 @@
+import{ax as t}from"./index-59d5b448.js";function n(){const{themeColor:r,primaryColorLevel:o}=t(),e=`${r}-${o}`;return{ringTheme:`ring-${e}`,borderTheme:`border-${e}`,bgTheme:`bg-${e}`,textTheme:`text-${e}`}}export{n as u};
