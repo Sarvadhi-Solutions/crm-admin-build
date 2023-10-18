7fcf5642171c55d350e255efcb076a3ac79fb8fe
maysolanki26 <mayur@sarvadhi.com>
2023-10-18 08:46:13 +0530

    New build

---
+++ assets/checkEditUpdate-2235bd5d.js
@@ -0,0 +1,1 @@
+import{u}from"./index-de229e0f.js";const i=(t,a)=>{const o=u(e=>{var s,r;return((r=(s=e.auth.user)==null?void 0:s.role)==null?void 0:r.rolePermissions)||[]});return o==null?void 0:o.some(e=>e.master.moduleName.toLowerCase()===t.toLowerCase()&&e[a])};export{i as u};
---
+++ assets/isNil-8888295d.js
@@ -0,0 +1,1 @@
+import{m as r}from"./index-de229e0f.js";function t(i){return i==null}var o=t;const l=r(o);export{l as i};
