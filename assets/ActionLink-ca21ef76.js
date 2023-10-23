9c6a81efe2c7e81389c0dfedf2d2145a701c3b84
maysolanki26 <mayur@sarvadhi.com>
2023-10-23 15:08:38 +0530

    New build changes

---
+++ assets/ActionLink-ca21ef76.js
@@ -0,0 +1,1 @@
+import{c as h,j as r,h as u}from"./index-3da8ab78.js";import{u as x}from"./useThemeClass-a75d86d3.js";const N=a=>{const{children:s,className:m,themeColor:n=!0,to:e,reloadDocument:c,href:l="",...o}=a,{textTheme:i}=x(),t={className:h(n&&i,"hover:underline",m)};return e?r.jsx(u,{to:e,reloadDocument:c,...t,...o,children:s}):r.jsx("a",{href:l,...t,...o,children:s})};export{N as A};
---
+++ assets/_getPrototype-cf8d7a10.js
@@ -0,0 +1,1 @@
+import{l as r}from"./Views-ac495012.js";var t=r,e=t(Object.getPrototypeOf,Object),a=e;export{a as _};
