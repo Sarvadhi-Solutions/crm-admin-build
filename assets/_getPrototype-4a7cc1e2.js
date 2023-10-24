4efc7662961bf4614f0cd73782ccbc7aec0ae95f
maysolanki26 <mayur@sarvadhi.com>
2023-10-24 09:06:18 +0530

    new build

---
+++ assets/_getPrototype-4a7cc1e2.js
@@ -0,0 +1,1 @@
+import{l as r}from"./Views-bc53fe60.js";var t=r,e=t(Object.getPrototypeOf,Object),a=e;export{a as _};
---
+++ assets/useUncertainRef-ccbee67f.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-a20ba26a.js";function u(e){const r=n.useRef(null);return e||r}export{u};
