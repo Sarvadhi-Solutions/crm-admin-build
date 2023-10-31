2c2a86466c32df2f2091d4f68d8c3a93484bca1d
maysolanki26 <mayur@sarvadhi.com>
2023-10-31 11:35:17 +0530

    New build

---
+++ assets/CloseButton-1ed6055a.js
@@ -0,0 +1,1 @@
+import{r as c,c as u,j as s}from"./index-349191dc.js";import{X as m}from"./index.esm-cb282f8a.js";const b=c.forwardRef((t,o)=>{const{absolute:e,className:a,defaultStyle:l,...n}=t,r=u("close-btn",l&&"close-btn-default",e&&"absolute z-10",a);return s.jsx("span",{className:r,role:"button",...n,ref:o,children:s.jsx(m,{})})});b.displayName="CloseButton";export{b as C};
