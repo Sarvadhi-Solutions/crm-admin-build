23af2f592d3e836fc31941076b58e2d8bfc2a192
maysolanki26 <mayur@sarvadhi.com>
2023-11-01 18:22:12 +0530

    new build

---
+++ assets/useThemeClass-ce525309.js
@@ -0,0 +1,1 @@
+import{ay as t}from"./index-01836369.js";function n(){const{themeColor:r,primaryColorLevel:o}=t(),e=`${r}-${o}`;return{ringTheme:`ring-${e}`,borderTheme:`border-${e}`,bgTheme:`bg-${e}`,textTheme:`text-${e}`}}export{n as u};
